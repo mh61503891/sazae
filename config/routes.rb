f4bdd95e4bb83f009e43732df370f77f2fe21de5
sakaguchi <takashi.sakaguchi@ummm.info>
2015-08-09 06:24:59 +0900

    せやなボタンの動作

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :houses, only: [:index, :show]
+  resources :houses, only: [:index, :show] do
+    resources :comments, only: [] do
+      member do
+        post :like, format: :json
+      end
+    end
+  end
 
   root 'welcome#index'
 end
