457787e883be02d97d9c11751bfa35fa7a261c3e
hiroyuki <ionis111@hotmail.com>
2015-08-09 11:32:53 +0900

    product-color定義！

--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,15 @@ ActiveRecord::Schema.define(version: 20150808204858) do
   end
 
   create_table "houses", force: :cascade do |t|
-    t.string   "title",                         null: false
-    t.string   "owner_type",                    null: false
+    t.string   "title",             null: false
+    t.string   "owner_type",        null: false
     t.string   "owner"
     t.string   "address"
     t.integer  "first_vacant_year"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "like_count",        default: 0, null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
 end
