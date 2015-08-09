35fd557bf51c7a6b44e25852f0ebeb928fd6537f
sakaguchi <takashi.sakaguchi@ummm.info>
2015-08-09 13:50:26 +0900

    コメント中の改行をbrに変換

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def controller_class_name
     [controller_name, action_name].join(' ')
   end
+
+  def br(string)
+    string = h(string) unless string.html_safe?
+    string.gsub(/(\r\n?)|(\n)/, "<br />\r\n").html_safe
+  end
 end
